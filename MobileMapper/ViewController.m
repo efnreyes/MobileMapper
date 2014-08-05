c693ad1c4ebab6f30d028353b90a0b25f793a953
Efrén Reyes <efren.reyes@globant.com>
2014-08-05 11:26:24 -0500

    Implemented delegate method to zoom in the map wher the i button is pressed

--- MobileMapper/ViewController.m
+++ MobileMapper/ViewController.m
@@ -27,6 +27,17 @@
     self.mobileMakersAnnotation.coordinate = coordinate;
     self.mobileMakersAnnotation.title = @"MOBILE MAKERS!";
     [self.myMapView addAnnotation:self.mobileMakersAnnotation];
+
+    NSString *address = @"Mount Rushmore";
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder geocodeAddressString:address completionHandler:^(NSArray *placemarks, NSError *error) {
+        for (CLPlacemark *place in placemarks) {
+            MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+            annotation.coordinate = place.location.coordinate;
+            annotation.title = place.subLocality;
+            [self.myMapView addAnnotation:annotation];
+        }
+    }];
 }
 
 //This delegate method should be implemented if you want to have a custom image instead of classic pin
@@ -36,6 +47,18 @@
     pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
     pin.image = [UIImage imageNamed:@"mobilemakers"];
     return pin;
+}
+
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+    CLLocationCoordinate2D centerCoordinate = view.annotation.coordinate;
+    MKCoordinateSpan coordinateSpan;
+    coordinateSpan.latitudeDelta = 0.01;
+    coordinateSpan.longitudeDelta = 0.01;
+    MKCoordinateRegion region;
+    region.center = centerCoordinate;
+    region.span = coordinateSpan;
+
+    [self.myMapView setRegion:region animated:YES];
 }
 
 @end
